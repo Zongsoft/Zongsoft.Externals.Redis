ee5e48223bac3afd8da798b2531c401a5ee6e553
Popeye Zhong <zongsoft@gmail.com>
2014-07-25 18:18:55 +0800

    Modify the AssemblyInfo.cs file.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Zongsoft.Externals.Redis")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Zongsoft Externals Redis Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Zongsoft.Externals.Redis")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Zongsoft Corporation")]
+[assembly: AssemblyProduct("Zongsoft.Externals.Redis Library")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2014. All rights reserved.")]
+[assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1407.*")]
+[assembly: AssemblyFileVersion("1.0.1407.0")]
