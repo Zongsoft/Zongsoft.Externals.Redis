cf08d22f9b95caf1e668b9eb7769ada388e30a10
Popeye Zhong <zongsoft@gmail.com>
2016-02-22 11:39:08 +0800

    :rocket:  增加一个nuget打包配置文件。 :racehorse:

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Zongsoft.Externals.Redis")]
-[assembly: AssemblyDescription("Zongsoft Externals Redis Library")]
+[assembly: AssemblyDescription("This is a library about Redis SDK.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zongsoft Corporation")]
 [assembly: AssemblyProduct("Zongsoft.Externals.Redis Library")]
-[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2014. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2014-2015. All rights reserved.")]
 [assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1407.*")]
+[assembly: AssemblyVersion("1.0.1407.0")]
 [assembly: AssemblyFileVersion("1.0.1407.0")]
