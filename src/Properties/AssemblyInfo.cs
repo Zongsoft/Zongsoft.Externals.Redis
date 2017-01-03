98c43bcc170b7de722d8331b4022c6d765a1822c
Popeye Zhong <zongsoft@gmail.com>
2017-01-04 02:12:36 +0800

    更新版本号。 :relaxed:

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zongsoft Corporation")]
 [assembly: AssemblyProduct("Zongsoft.Externals.Redis Library")]
-[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2014-2015. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2014-2016. All rights reserved.")]
 [assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1407.0")]
-[assembly: AssemblyFileVersion("1.0.1407.0")]
+[assembly: AssemblyVersion("1.1.0.1611")]
+[assembly: AssemblyFileVersion("1.1.0.1611")]
